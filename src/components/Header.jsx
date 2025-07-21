2cd5d7d69b29d49e53d18f5b49ce2c49357f1b3f
costabile12 <franco_costabile@hotmail.com>
2025-07-21 17:37:30 -0300

    cambie en las queris del menu activado que no se muestre si la pantalla es mas grande de 992px

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import LogoElectroFix from "../assets/images/logo.png";
 // --Hoja de estilos--
 import "../styles/Header.css";
 
 import Boton from "./Boton";
 import { FaBars } from "react-icons/fa"; // ícono de menú
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 function Header() {
 
+  // El valor del estado inicial es flaso para que en la condicion ternaria no muestre el menu
   const [menuAbierto, setMenuAbierto] = useState(false)
 
-  const toggleMenu = () => {
-    setMenuAbierto(!menuAbierto);
-  }
+  const location = useLocation(); // Importante
+
+
+  //  Cierra el menú automáticamente cuando cambia la ruta
+  useEffect(() => {
+    setMenuAbierto(false)
+  }, [location]);
 
   return(
     <header className="encabezado">
@@ -35,13 +41,23 @@ function Header() {
             <li><Link to="/servicios">Servicios</Link></li>
             <li><Link to="/quienes-somos">¿Quiénes Somos?</Link></li>
             <li><Link to="/contacto">Contacto</Link></li>
+
+          {/* import { Link } from "react-router-dom";
+
+            Esta era otra opcion mas simple
+
+          <li><Link to="/" onClick={() => setMenuAbierto(false)}>Inicio</Link></li>
+          <li><Link to="/productos" onClick={() => setMenuAbierto(false)}>Productos</Link></li>
+          <li><Link to="/contacto" onClick={() => setMenuAbierto(false)}>Contacto</Link></li> */}
+
           </ul>
         </nav>
 
       <Boton
         className="menu-navegacion"
         icono={<FaBars size={24} />}
-        onClick={toggleMenu}
+        // Niega la condicion para que cuando sea verdadero agregue o no la clase activo
+        onClick={() => setMenuAbierto(!menuAbierto)}
       />
     </div>
 
